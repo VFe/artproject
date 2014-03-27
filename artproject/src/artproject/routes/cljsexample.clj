f8af3a16a04815aadefa400085ac0604813d5491
Volker Einsfeld <volker@perfectparallel.com>
2014-03-27 11:58:16 -0400

    Adding backend beginnings

---
+++ artproject/src/artproject/routes/cljsexample.clj
@@ -0,0 +1,16 @@
+(ns artproject.routes.cljsexample
+  (:require [compojure.core :refer :all]
+            [artproject.views.layout :as layout]))
+
+(def messages
+  (atom 
+    [{:message "Hello world"
+      :user    "Foo"}
+     {:message "Ajax is fun"
+      :user    "Bar"}]))
+
+(defroutes cljs-routes
+  (GET "/cljsexample" [] (layout/render "cljsexample.html"))
+  (GET "/messages" [] {:body @messages})
+  (POST "/add-message" [message user]
+        {:body (swap! messages conj {:message message :user user})}))
---
+++ artproject/src/artproject/routes/home.clj
@@ -0,0 +1,15 @@
+(ns artproject.routes.home
+  (:use compojure.core)
+  (:require [artproject.views.layout :as layout]
+            [artproject.util :as util]))
+
+(defn home-page []
+  (layout/render
+    "home.html" {:content (util/md->html "/md/docs.md")}))
+
+(defn about-page []
+  (layout/render "about.html"))
+
+(defroutes home-routes
+  (GET "/" [] (home-page))
+  (GET "/about" [] (about-page)))
